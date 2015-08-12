20c55783f5d8287f4b7f04be5281dcc94a50c154
Toomas Vendelin <toomas@vendelin.com>
2015-08-12 10:39:23 +0200

    Added self.instances class instance method to rabbitmq_vhost provider.

--- modules/_ext/lib/puppet/provider/rabbitmq_vhost/rabbitmqctl.rb
+++ modules/_ext/lib/puppet/provider/rabbitmq_vhost/rabbitmqctl.rb
@@ -14,4 +14,24 @@ Puppet::Type.type(:rabbitmq_vhost).provide :rabbitmqctl do
 		! ( rabbitmqctl '-q',  'list_vhosts' ).split("\n").grep( @resource[:name] ).empty?
 	end
 	
+	def self.instances
+		Puppet.debug "Running self.instances method"
+		instances = []
+		get_vhosts.map do |vhost|
+			new( :name => vhost.to_s, :ensure => :present )
+		end
+	end
+	
+	
+	# Helper methods
+	
+	def self.get_vhosts
+		begin
+			( rabbitmqctl '-q',  'list_vhosts' ).split("\n")
+		rescue => ex
+			Puppet.warning "Could not prefetch vhosts. Exception:\n%s" % ex.to_s
+			return []
+		end
+	end
+	
 end 
